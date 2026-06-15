03485809282caffcca3dcd3445249265cd903f8e
Don-Marivate <150945172+Don-Marivate@users.noreply.github.com>
2026-06-15 18:00:30 +0200

    experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,28 +5,21 @@ import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a passionate and versatile software engineer with a knack for building seamless and intuitive web applications. With two years of hands on experience, I’ve dived into a dynamic tech stack, mastering Python, Django, React, Node.js, SQL, and Java. My fascination with how things work evolved into a thriving career where I embrace every challenge as an opportunity to innovate and grow. I thrive in collaborative settings, turning complex problems into elegant solutions. When I’m not coding, you’ll find me exploring the latest tech trends, contributing to open-source projects, or staying active to keep both mind and body sharp.`;
+export const ABOUT_TEXT = `I am a versatile software engineer with a knack for building seamless and intuitive web applications. With two years of hands on experience, I’ve dived into a dynamic tech stack, mastering Python, Django, React, Node.js, SQL, and Java. My fascination with how things work evolved into a thriving career where I embrace every challenge as an opportunity to innovate and grow. I thrive in collaborative settings, turning complex problems into elegant solutions. When I’m not coding, you’ll find me exploring the latest tech trends, contributing to open-source projects, or staying active to keep both mind and body sharp.`;
 
 export const EXPERIENCES = [
   {
-    year: "April 2025 - October 2025",
-    role: "Software Engineer",
-    company: "SoftWave Technology",
-    description: `During my work-integrated learning at Softwave Technology, I designed and implemented comprehensive book management features to enhance the efficiency of library operations. I developed user registration and book management modules that streamlined user access and resource organization. Additionally, I implemented transaction processing for book borrowing and returns, ensuring accurate reporting and smooth day to day library operations.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "May 2024 - September",
-    role: "CTU",
+    year: "June 2025 - October 2025",
+    role: "Coast IT",
     company: "Software Engineer",
     description: `Designed and developed responsive full-stack web applications using React.js, Next.js, Node.js, and Django, ensuring modularity, scalability, and maintainability. Integrated RESTful and GraphQL APIs, optimized database queries, and enhanced application performance to deliver seamless user experiences. Collaborated with clients to gather requirements for projects serving between 1,000 and 5,000 users monthly. Deployed and maintained applications on Azure and AWS, achieving 99% uptime across all client projects.`,
     technologies: ["Javascript", "React.js", "Next.js", "Node.js", "SQL"],
   },
   {
-    year: "February 2023 - March 2024",
-    role: "Software Engineer",
-    company: "KATIBA.",
-    description: `Contributed to the development and maintenance of software systems as a trainee using Python, Django, React, Node.js, and SQL. Implemented RESTful APIs and integrated with SQL databases. Worked closely with stakeholders to define project requirements and timelines.`,
+    year: "June 2024 - October 2024",
+    role: "Systems Developer",
+    company: "SoftWave Technology",
+    description: `During my work-integrated learning at Softwave Technology, I designed and implemented comprehensive book management features to enhance the efficiency of library operations. I developed user registration and book management modules that streamlined user access and resource organization. Additionally, I implemented transaction processing for book borrowing and returns, ensuring accurate reporting and smooth day to day library operations.`,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
 ];
