8bb3681d7b438272f4c2f4c2cb2c6e36b747cdeb
Don-Marivate <150945172+Don-Marivate@users.noreply.github.com>
2026-06-15 19:05:13 +0200

    reverted

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Experience from "./components/Experience";
 
+
 const App = () => {
   return (
-    <div className="min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-      
-      {/* Navbar */}
-      <Navbar />
-
-      {/* Main Content */}
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-0 -z-10 h-full w-full">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
       <div className="container mx-auto px-8">
+        <Navbar />
         <Hero />
-        <About />
+        <About /> 
         <Technologies />
         <Experience />
         <Projects />
