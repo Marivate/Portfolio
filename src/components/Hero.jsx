fd00f6fca6ffaad59d503a3a9dca6661064171ec
Tintswalo Marivate <150945172+Marivate@users.noreply.github.com>
2026-06-25 16:54:19 +0200

    Import image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import profilePic from "../assets/tintswaloProfile.jpeg";
+import profilePic from "../assets/tintswaloprofile.jpeg";
 import { motion } from "framer-motion"
 
 const container = (delay) => ({
